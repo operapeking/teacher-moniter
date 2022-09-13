f0fc5d824dbf32d563f36eaff8bc2882d219854a
mklzc <mklzc102@qq.com>
2022-09-13 11:04:18 +0800

    修复了已知错误

--- toast/toast.py
+++ toast/toast.py
@@ -1,36 +1,31 @@
+"""
+Note:
+Use Command "nohup" to run in the background like:
+nohup python toast.py & >toast.log
+use Command "jobs" to check whether it is running successfully
+"""
 import requests
 import platform
 import os
-
-if platform.system() == "Windows":
-    import win10toast
-    toaster = win10toast.ToastNotifier()
+import time
+if platform.system() != "Linux":
+    print("Program can only run in linux system.")
+    exit(0)
 last = 2
-system = platform.system()
-def WindowsDanger():
-    toaster.show_toast('Warning', 'Someone is coming')
-def WindowsSafe():
-    toaster.show_toast('Safe', 'No one is around')
-def LinuxDanger():
-    os.system('notify-send "Someone is coming"')
-def LinuxSafe():
-    os.system('notify-send "No one is around"')
+url = "http://172.100.109.135:10086/query"
+def ChangeLog(data):
+    print("{} changed status to {} in {}".format(data["ip"], data["isComing"], data["time"]))
 def Check(Last, Now):
     if Last == 1 and Now == 0:
-        if platform.system() == "Windows":
-            WindowsSafe()
-        if platform.system() == "Linux":
-            LinuxSafe()
+        os.system('notify-send "No one is around"')
+        return True
     if Last == 0 and Now == 1:
-        if platform.system() == "Windows":
-            WindowsDanger()
-        if platform.system() == "Linux":
-            LinuxDanger()
+        os.system('notify-send "Someone is coming"')
+        return True
+    return False
 while True:
-    try:
-        data = requests.get("http://172.100.109.135:10086/query").json()
-        Check(last, data["isComing"])
-        last = data["isComing"]
-    except:
-        print("Exit")
-        exit(1)
+    time.sleep(0.1)
+    data = requests.get(url).json()
+    if Check(last, data["isComing"]) == True:
+        ChangeLog(data)
+    last = data["isComing"]
