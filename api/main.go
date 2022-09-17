4e0ae5d54cb0c42f794ce59c80bcfe704f45e8b3
operapeking <pekingopera@protonmail.com>
2022-09-17 11:46:14 +0800

    修复 api 的错误

--- api/main.go
+++ api/main.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"flag"
+	"fmt"
 	"net/http"
 )
 
@@ -22,12 +23,20 @@ func SendModify() {
 }
 
 func main() {
-	var ip, port string
+	var ip, port, op string
 	flag.StringVar(&ip, "ip", "127.0.0.1", "server ip")
 	flag.StringVar(&port, "port", "10086", "server port")
-	flag.BoolVar(&send.IsComing, "op", false, "1 is coming, 0 is gone")
-	flag.StringVar(&send.Who, "who", "none", "who is coming")
+	flag.StringVar(&op, "op", "0", "1 is coming, 0 is gone")
+	flag.StringVar(&send.Who, "who", "someone", "who is coming")
 	flag.Parse()
 	host = "http://" + ip + ":" + port
+	if op == "1" {
+		send.IsComing = true
+	} else if op == "0" {
+		send.IsComing = false
+	} else {
+		fmt.Println("op 参数输入错误")
+		return
+	}
 	SendModify()
 }
